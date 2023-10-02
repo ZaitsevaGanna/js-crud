00611e906ae3f516ac9ea3c0c20aa55ec790b82f
Ganna Zaitseva <ganna.zajtseva@gmail.com>
2023-10-02 18:09:51 +0300

    crud-product

--- src/route/index.js
+++ src/route/index.js
@@ -72,13 +72,6 @@ class User {
     }
   }
 
-  // if (!user || !email) {
-  //   return false
-  // }
-
-  // user.email = email
-  // return true
-
   static update = (user, { email }) => {
     if (email) {
       user.email = email
@@ -103,7 +96,6 @@ router.get('/user-delete', function (req, res) {
 
   User.deleteById(Number(id))
 
-  //console.log(id)
   res.render('success-info', {
     style: 'success-info',
     info: 'Користувач видалений',
@@ -126,6 +118,145 @@ router.post('/user-update', function (req, res) {
     info: result
       ? 'Email пошта оновлена'
       : 'Сталася помилка',
+  })
+})
+
+//=========================================================
+//===========================================================
+router.get('/product-create', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('product-create', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-create',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+class Product {
+  static #list = []
+
+  constructor(name, price, description) {
+    this.id = Math.floor(Math.random() * 100000)
+    this.createDate = new Date().toISOString()
+    this.name = name
+    this.price = price
+    this.description = description
+  }
+  static getList = () => {
+    return this.#list
+  }
+  static addProduct = (product) => {
+    return this.#list.push(product)
+  }
+  static getById = (id) => {
+    return this.#list.find((product) => product.id === id)
+  }
+
+  static deleteById = (id) => {
+    console.log('ID:', id)
+    const index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+    console.log(index)
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      console.log(this.#list)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static updateById = (id, data) => {
+    const product = this.getById(id)
+
+    if (product) {
+      this.update(product, data)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static update(product, { name, price, description }) {
+    product.name = name
+    product.price = price
+    product.description = description
+  }
+}
+
+router.post('/product-create', function (req, res) {
+  const { name, price, description } = req.body
+  const product = new Product(name, price, description)
+  Product.addProduct(product)
+
+  res.render('alert', {
+    style: 'alert',
+    info: 'Товар створений',
+  })
+})
+
+router.get('/product-list', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  const list = Product.getList()
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('product-list', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-list',
+
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/product-edit', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  const { id } = req.query
+
+  const product = Product.getById(Number(id))
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('product-edit', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-edit',
+
+    data: {
+      product,
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/product-delete', function (req, res) {
+  const { id } = req.query
+
+  Product.deleteById(Number(id))
+
+  res.render('alert', {
+    style: 'alert',
+    info: 'Товар видалений',
+  })
+})
+
+router.post('/product-edit', function (req, res) {
+  const { name, price, description, id } = req.body
+
+  const product = Product.getById(Number(id))
+
+  Product.update(product, { name, price, description })
+
+  res.render('alert', {
+    style: 'alert',
+    info: 'Товар оновлен',
   })
 })
 
